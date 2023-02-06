31c3a15b0874cadec13897330f0dcbea7ce893ae
grupoalcoset <124085376+grupoalcoset@users.noreply.github.com>
2023-02-06 20:42:32 +0000

    João Brito - Add all quiz functionality

--- trabalho/trabalho/Form1.Designer.vb
+++ trabalho/trabalho/Form1.Designer.vb
@@ -22,54 +22,97 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.components = New System.ComponentModel.Container()
+        Me.Titulo = New System.Windows.Forms.Label()
+        Me.Começar = New System.Windows.Forms.Button()
+        Me.Regras = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Começar2 = New System.Windows.Forms.Button()
+        Me.Começar3 = New System.Windows.Forms.Button()
+        Me.start_button = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Label1
+        'Titulo
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(372, 80)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(32, 15)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Jogo"
+        Me.Titulo.AutoSize = True
+        Me.Titulo.Location = New System.Drawing.Point(648, 245)
+        Me.Titulo.Name = "Titulo"
+        Me.Titulo.Size = New System.Drawing.Size(140, 15)
+        Me.Titulo.TabIndex = 0
+        Me.Titulo.Text = "Qual é o time de futebol?"
         '
-        'Button1
+        'Começar
         '
-        Me.Button1.Location = New System.Drawing.Point(580, 238)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "comecar"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Começar.Location = New System.Drawing.Point(338, 126)
+        Me.Começar.Name = "Começar"
+        Me.Começar.Size = New System.Drawing.Size(112, 23)
+        Me.Começar.TabIndex = 1
+        Me.Começar.Text = "Comecar Fácil"
+        Me.Começar.UseVisualStyleBackColor = True
         '
-        'Button2
+        'Regras
         '
-        Me.Button2.Location = New System.Drawing.Point(276, 232)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "regras"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.Regras.Location = New System.Drawing.Point(684, 350)
+        Me.Regras.Name = "Regras"
+        Me.Regras.Size = New System.Drawing.Size(75, 23)
+        Me.Regras.TabIndex = 2
+        Me.Regras.Text = "Regras"
+        Me.Regras.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Começar2
+        '
+        Me.Começar2.Location = New System.Drawing.Point(338, 164)
+        Me.Começar2.Name = "Começar2"
+        Me.Começar2.Size = New System.Drawing.Size(112, 23)
+        Me.Começar2.TabIndex = 3
+        Me.Começar2.Text = "Começar Méido"
+        Me.Começar2.UseVisualStyleBackColor = True
+        '
+        'Começar3
+        '
+        Me.Começar3.Location = New System.Drawing.Point(338, 208)
+        Me.Começar3.Name = "Começar3"
+        Me.Começar3.Size = New System.Drawing.Size(112, 23)
+        Me.Começar3.TabIndex = 4
+        Me.Começar3.Text = "Começar Dificil"
+        Me.Começar3.UseVisualStyleBackColor = True
+        '
+        'start_button
+        '
+        Me.start_button.Location = New System.Drawing.Point(684, 294)
+        Me.start_button.Name = "start_button"
+        Me.start_button.Size = New System.Drawing.Size(75, 23)
+        Me.start_button.TabIndex = 5
+        Me.start_button.Text = "Iniciar"
+        Me.start_button.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.start_button)
+        Me.Controls.Add(Me.Começar3)
+        Me.Controls.Add(Me.Começar2)
+        Me.Controls.Add(Me.Regras)
+        Me.Controls.Add(Me.Começar)
+        Me.Controls.Add(Me.Titulo)
         Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Button1 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents Titulo As Label
+    Friend WithEvents Começar As Button
+    Friend WithEvents Regras As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Começar2 As Button
+    Friend WithEvents Começar3 As Button
+    Friend WithEvents start_button As Button
 End Class
